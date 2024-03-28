af6853c3e9608e296d58c0543cb29e34c6519b3a
Brotastor12 <astormbelizzi@gmail.com>
2024-03-28 10:52:30 -0300

    electron + carrito

--- Venta.js
+++ Venta.js
@@ -1,5 +1,25 @@
-class Caja{
+
+class Carro{
     constructor(){
-        
+        this.lista = {};
+        this.precioTotal = 0;
     }
+    agregarProducto(prod, cant){
+        if(this.lista[prod] != null){
+            this.lista[prod] += cant;
+        } else{
+            this.lista[prod] = cant;
+        }
+        this.precioTotal += prod.precio * cant;
+    }
+    get_prod(indice){
+        this.lista[indice];
+    }
+    print_carro(){
+        for(let i = 0; i< this.lista.length; i++){
+            console.log(this.lista[i].nombre + ' ' + this.lista[i][this.lista[i]]);
+        }
+        console.log(this.precioTotal);
+    }
+    
 }
